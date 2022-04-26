d8baee1743e09d095d3e2971fb4a415abdebce22
Aleksandr Eschenko <101901195+aleksEschenko@users.noreply.github.com>
2022-04-27 01:26:10 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ navIcon.addEventListener('click', function () {
 	this.classList.toggle('nav-icon--active');
 	navMobile.classList.toggle('nav-mobile--active');
 });
+
+const navLinks = document.querySelectorAll('.nav-mobile a');
+
+navLinks.forEach(function (item) {
+    // для каждой ссылки добавляем прослушку по событию "клик"
+    item.addEventListener('click', function () {
+        navIcon.classList.remove('nav-icon--active'); //убираем активный класс у иконки моб навигации
+        navMobile.classList.remove('nav-mobile--active'); // убираем активный класс у блока моб навигации
+    })
+})
